0b2a2cd7051fdb79706740cd9244ac24622219c0
Duy Thinh Dinh <dd9692@g.rit.edu>
2026-02-23 20:09:02 -0500

    git update db_utils

--- server/api/db_utils.py
+++ server/api/db_utils.py
@@ -3,6 +3,20 @@ import yaml
 import os
 
 def connect():
+    db_user = os.environ.get('DB_USER')
+    db_password = os.environ.get('DB_PASSWORD')
+    db_host = os.environ.get('DB_HOST')
+    db_port = os.environ.get('DB_PORT')
+    db_name = os.environ.get('DB_NAME')
+
+    if db_user and db_password and db_host:
+        return psycopg2.connect(
+            dbname=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port
+        )
     config = {}
     yml_path = os.path.join(os.path.dirname(__file__), 'db.yml')
     with open(yml_path, 'r') as file:
